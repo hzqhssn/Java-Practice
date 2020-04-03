b9669f019afb779643e1848dc4ed65643eb946fb
Haziq <haziq.hassan@cyberneticentertainment.com>
2020-04-02 22:21:27 -0500

    AreaCalculator

---
+++ Section 4/AreaCalculator/src/AreaCalculator.java
@@ -0,0 +1,30 @@
+public class AreaCalculator {
+
+    public static void main(String[] args)
+    {
+        System.out.println(area(5));
+    }
+
+    public static double area (double radius)
+    {
+        if(radius >= 0)
+        {
+         return Math.PI*radius*radius;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+
+    public static double area (double x, double y)
+    { if(x >= 0 && y >= 0)
+    {
+        return  x * y;
+    }
+    else
+    {
+        return -1;
+    }
+    }
+}
