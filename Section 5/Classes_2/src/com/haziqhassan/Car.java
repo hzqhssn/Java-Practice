308338e109aa1076af673b4ee6d329acae8f281f
Haziq <haziq.hassan@cyberneticentertainment.com>
2020-04-08 18:22:51 -0500

    Classes

---
+++ Section 5/Classes_2/src/com/haziqhassan/Car.java
@@ -0,0 +1,17 @@
+package com.haziqhassan;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String color;
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+}
