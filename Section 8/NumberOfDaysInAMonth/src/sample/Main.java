146125b6f8d2c69a1839729b48296f36260d1a0e
Haziq <haziq.hassan@cyberneticentertainment.com>
2020-05-31 17:22:05 -0500

    Number of days in a month

--- Section 8/NumberOfDaysInAMonth/src/sample/Main.java
+++ Section 8/NumberOfDaysInAMonth/src/sample/Main.java
@@ -6,18 +6,45 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main  {
+ public static void main(String[] args)
+ {
+     System.out.println(isLeapYear(1250));
+     System.out.println(getDaysInMonth(2,1251));
+    }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public static boolean isLeapYear (int year)
+    {
+        if(year < 1 || year > 9999 )
+        {
+            return false;
+        }
+        else if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            return true;
+        }
+        else
+            {
+            return false;
+        }
+    }
+
+    public  static  int getDaysInMonth(int month, int year) {
+        if (month < 1 || month > 12) {
+            return -1;
+        } else if (year < 1 || year > 9999) {
+            return -1;
+        } else if (isLeapYear(year) && month == 2) {
+            return 29;
+        } else if (!isLeapYear(year) && month == 2) {
+            return 28;
+        } else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+            return 31;
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            return 30;
+        }else
+        {return -1;}
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
